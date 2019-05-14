353e265c0f2611e785fd78dffa0dabacf8a4c529
Your Name <osv1313006@gmail.com>
2019-05-14 12:24:02 +0300

    currency

--- NewsPage/js/main.js
+++ NewsPage/js/main.js
@@ -1,20 +1,8 @@
-// set endpoint and your access key
-endpoint = 'latest';
-access_key = 'cb78031db0bd2804a418035df2808fbc';
-
-// define from currency, to currency, and amount
-from = 'EUR';
-to = 'GBP';
-amount = '10';
-
-// execute the conversion using the "convert" endpoint:
-$.ajax({
-  url: 'http://data.fixer.io/api/' + endpoint + '?access_key=' + access_key + '&from=' + from + '&to=' + to + '&amount=' + amount,
-  dataType: 'jsonp',
-  success: function (json) {
-
-    // access the conversion result in json.result
-    console.log(json);
-
-  }
+$(function(){
+	$.get('http://data.fixer.io/api/latest?symbols=USD,RUB', {'access_key': 'cb78031db0bd2804a418035df2808fbc'}, function(response){
+		var RUB = '<span class="rub">' + 'EUR/RUB: ' + (response.rates.RUB) + '</span><br>';
+		var USD = '<span class="usd">' + 'EUR/USD: ' + (response.rates.USD) + '</span>';
+		$('.course-app').append(RUB);
+		$('.course-app').append(USD);
+		});
 });
