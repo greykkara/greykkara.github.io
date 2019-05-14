17e38dcf7a28ec03607b4f94cefe70a5b01b8112
Your Name <osv1313006@gmail.com>
2019-05-14 16:06:32 +0300

    scroll_disabling_onclick

--- NewsPage/js/main.js
+++ NewsPage/js/main.js
@@ -1,13 +1,21 @@
-$(function(){
-	$.get('http://data.fixer.io/api/latest?symbols=USD,RUB', {'access_key': 'cb78031db0bd2804a418035df2808fbc'}, function(response){
+$(function () {
+	$.get('http://data.fixer.io/api/latest?symbols=USD,RUB', {
+		'access_key': 'cb78031db0bd2804a418035df2808fbc'
+	}, function (response) {
 		var RUB = '<span class="rub">' + 'EUR/RUB: ' + (response.rates.RUB) + '</span><br>';
 		var USD = '<span class="usd">' + 'EUR/USD: ' + (response.rates.USD) + '</span>';
 		$('.course-app').append(RUB);
 		$('.course-app').append(USD);
-		});
+	});
 
-		$("a").click(function(event) {
-    
-			event.preventDefault();
+	$("a").click(function (event) {
+		event.preventDefault();
+	});
+
+});
+
+$(document).ready(function(){
+	$(".all-news").on("click", function () {
+		$("html, body").toggleClass("scroll-dis");
 	});
 });
