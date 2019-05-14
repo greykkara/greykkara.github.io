d941302cb59fc0ce773cb7705c65bda7dc7201f5
Your Name <osv1313006@gmail.com>
2019-05-14 11:57:58 +0300

    sfs

--- NewsPage/js/main.js
+++ NewsPage/js/main.js
@@ -2,21 +2,19 @@
 endpoint = 'latest';
 access_key = 'cb78031db0bd2804a418035df2808fbc';
 
-// get the most recent exchange rates via the "latest" endpoint:
 // define from currency, to currency, and amount
-from = 'USD';
-to = 'RUB';
-amount = '1';
+from = 'EUR';
+to = 'GBP';
+amount = '10';
 
 // execute the conversion using the "convert" endpoint:
 $.ajax({
   url: 'http://data.fixer.io/api/' + endpoint + '?access_key=' + access_key + '&from=' + from + '&to=' + to + '&amount=' + amount,
   dataType: 'jsonp',
-  success: function (json) {}
+  success: function (json) {
+
+    // access the conversion result in json.result
+    console.log(json);
+
+  }
 });
-// $(document).ready(function () {
-//   $('.currensy').text(function ();
-
-//   );
-
-// });
