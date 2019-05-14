3553c8916b442b323b147512fdfb25587b85a9e6
Your Name <osv1313006@gmail.com>
2019-05-14 12:32:34 +0300

    currencyyy

--- NewsPage/js/main.js
+++ NewsPage/js/main.js
@@ -1,5 +1,5 @@
 $(function(){
-	$.get('https://data.fixer.io/api/latest?symbols=USD,RUB', {'access_key': 'cb78031db0bd2804a418035df2808fbc'}, function(response){
+	$.get('http://data.fixer.io/api/latest?symbols=USD,RUB', {'access_key': 'cb78031db0bd2804a418035df2808fbc'}, function(response){
 		var RUB = '<span class="rub">' + 'EUR/RUB: ' + (response.rates.RUB) + '</span><br>';
 		var USD = '<span class="usd">' + 'EUR/USD: ' + (response.rates.USD) + '</span>';
 		$('.course-app').append(RUB);
